13cc735bd7fa7062c2f8b87c8f6e581b08cee8d2
sergey.klymenko <skl>
2024-08-19 13:45:00 +0300

    init

---
+++ app/src/main/java/com/ksyen/movies_redux/VideosActivity.kt
@@ -0,0 +1,47 @@
+package com.ksyen.movies_redux
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.ksyen.movies_redux.ui.theme.Movies_reduxTheme
+
+class VideosActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Movies_reduxTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Movies_reduxTheme {
+        Greeting("Android")
+    }
+}
---
+++ app/src/main/java/com/ksyen/movies_redux/VideosTabbar.kt
@@ -0,0 +1,10 @@
+package com.ksyen.movies_redux
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.compose.rememberNavController
+
+@Composable
+fun VideosBottomBar(modifier: Modifier) {
+    val navController = rememberNavController()
+}
